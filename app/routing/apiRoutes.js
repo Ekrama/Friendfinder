d25da5958f5bfb183dfb0a0191a0c1e7d4a0fd91
Ekrama Omar <47907934+Ekrama@users.noreply.github.com>
2019-06-11 13:22:42 -0500

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,31 +7,34 @@ module.exports = function(app) {
     });
 
 
-app.post('/api/friends', function(req,res){
-var compatibleFriend = findMostCompatableFriends (req);
-friendArray.push(req.body);
-res.json(compatibleFriend);
-})
+    app.post('/api/friends', function(req,res){
+        var compatibleFriend = findMostCompatableFriends (req);
+        friendArray.push(req.body);
+        res.json(compatibleFriend);
+    })
 };
 
 function findMostCompatableFriends(req) {
-var arrrayToHoldDiffernces = [];
-//goes through the people that have taken the survey before.
-for(var i =0;i <friendArray.length; i++)
-{
-    var totalDifference = 0;
-// for (start; end; increment)
+    var arrrayToHoldDiffernces = [];
+    //console.log(friendArray);
+    //goes through the people that have taken the survey before.
+    console.log(friendArray.length);
+    let minDifference = Infinity;
+    let friendIndex = 0;
+    for(var i=0; i < friendArray.length; i++){
+        var totalDifference = 0;
+    // for (start; end; increment)
+        console.log(friendArray[i]);
+        for(var j=0; j<friendArray[i].scores.length; j++){
+            totalDifference += Math.abs(req.body.scores[j] -friendArray[i].scores[j]);
+        }
 
-    for(var j=0; j<friendArray[i].scores.length; j++)
-{
-   totalDifference += Math.abs(req.body.scores[j] -friendArray[i].scores[j]);
-}
-
-arrrayToHoldDiffernces.push(totalDifference);
-[2,3,4,5]
-mostcompatibleMatchIndex = arrrayToHoldDiffernces.indexOf(math.apply (math, arrrayToHoldDiffernces));
-    return friendArray[mostcompatibleMatchIndex]
-}
+        if (totalDifference < minDifference){
+            minDifference = totalDifference;
+            friendIndex = i;
+        }
+        return friendArray[friendIndex];
+    }
 
 //hold the diff btn the users 
 };
